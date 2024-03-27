61cf0c43def2b522cb24307b37e30fc430d1af42
KomaromiJano <komaromijano2002@gmail.com>
2024-03-27 08:17:10 +0100

    First commit

---
+++ kezirat/Kezirat.cs
@@ -0,0 +1,22 @@
+﻿namespace kezirat;
+
+public class Kezirat
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+    public int PageCount { get; set; }
+    
+    public Kezirat(string title, string author, int year, int pageCount)
+    {
+        Title = title;
+        Author = author;
+        Year = year;
+        PageCount = pageCount;
+    }
+    
+    public override string ToString()
+    {
+        return $"{Title} ({Year}) by {Author} - {PageCount} pages";
+    }
+}
